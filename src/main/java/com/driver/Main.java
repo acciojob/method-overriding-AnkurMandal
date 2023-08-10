d47affe5949619ddea9967f63da98a8c224c3804
Ankur <ankurmandal.official@gmail.com>
2023-08-10 19:20:31 +0530

    Method Overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB=new B();
+        String ans=objB.meth();
+        System.out.println(ans);
+    }
 }
